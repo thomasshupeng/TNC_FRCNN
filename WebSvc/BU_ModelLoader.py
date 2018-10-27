016b7b4d47ae88043c8da63a877496b58be44927
Shu Peng <shpeng@microsoft.com>
2018-10-26 17:46:07 -0700

    Loading FRCNN model.

---
+++ WebSvc/BU_ModelLoader.py
@@ -0,0 +1,65 @@
+import os
+
+# load configs for detector, base network and data set
+from FasterRCNN.FasterRCNN_config import cfg as detector_cfg
+# AlexNet base model
+from utils.configs.AlexNet_config import cfg as network_cfg
+# BU (Beijing University) data set
+from utils.configs.BU_config import cfg as dataset_cfg
+from utils.config_helpers import merge_configs
+
+import numpy as np
+import utils.od_utils as od
+from FasterRCNN.FasterRCNN_train import prepare
+from cntk import load_model
+
+_image_height = 512
+_image_width = 682
+
+
+class FRCNN_Model:
+    def __init__(self, model_type):
+        # super().__init__()
+
+        self.cfg = merge_configs([detector_cfg, network_cfg, dataset_cfg, {'DETECTOR': 'FasterRCNN'}])
+
+        self._base_model_name = "faster_rcnn_eval_AlexNet_e2e_native.model"
+        self.name = "BU_" + self._base_model_name
+        self.model_path = os.path.join(os.path.join(os.path.dirname(__file__), 'models'), model_type)
+        self.model_file = os.path.join(self.model_path, self.name)
+        print("Model file:{}".format(self.model_file))
+        self.cfg['DATA'].MAP_FILE_PATH = self.model_path
+        print("Class map file path {}".format(self.cfg['DATA'].MAP_FILE_PATH))
+        self.eval_model = None
+        return
+
+    def load(self):
+        prepare(self.cfg, use_arg_parser=False)
+        print("Loading existing model from %s" % self.model_file)
+        self.eval_model = load_model(self.model_file)
+        return
+
+    def predict(self, image_path):
+        predictions = self._eval_single_image(image_path)
+        return predictions
+
+    # Evaluates a single image using the provided model
+    def _eval_single_image(self, img_path):
+        from FasterRCNN.FasterRCNN_eval import FasterRCNN_Evaluator
+        evaluator = FasterRCNN_Evaluator(self.eval_model, self.cfg)
+        regressed_rois, cls_probs = evaluator.process_image(img_path)
+        bboxes, labels, scores = od.filter_results(regressed_rois, cls_probs, self.cfg)
+
+        # write detection results to output
+        fg_boxes = np.where(labels > 0)
+        print("#bboxes: before nms: {}, after nms: {}, foreground: {}".format(len(regressed_rois), len(bboxes),
+                                                                              len(fg_boxes[0])))
+        fg = fg_boxes[0]
+        predictions = []
+        for i in fg:
+            print("{:<12} (label: {:<2}), score: {:.3f}, box: {}".format(
+                self.cfg["DATA"].CLASSES[labels[i]], labels[i], scores[i], [int(v) for v in bboxes[i]]))
+            predictions.append({"TagId": np.asscalar(labels[i]),
+                                "Tag": self.cfg["DATA"].CLASSES[labels[i]],
+                                "Probability": np.asscalar(scores[i])})
+        return predictions
---
+++ WebSvc/TNC_RestSvc.py
@@ -0,0 +1,170 @@
+"""
+File Name: TNC_RestSvc.py
+v 1.0
+
+This program provides the RESTful API for TNC project
+
+6/30/2018
+Shu Peng
+"""
+from flask import Flask, jsonify, request
+import os
+import requests
+import datetime
+import shutil
+import tempfile
+import BU_ModelLoader
+
+'''
+1.	PredictImageUrl
+https://southcentralus.dev.cognitive.microsoft.com/docs/services/57982f59b5964e36841e22dfbfe78fc1/operations/5a3044f608fa5e06b890f163
+                
+This one is predicting image by a given a url, which is very similar to what you have with some changes:
+a.	Instead pass pic URL as url parameter, it passes URL in http body, which doesn’t need any encoding.  
+b.	We can make projectid, iterationid, applicationid all as optional, those id can be useful for different 
+projects(云南老君山，北大生命科学院， etc.)
+
+2.	PredictImage
+https://southcentralus.dev.cognitive.microsoft.com/docs/services/57982f59b5964e36841e22dfbfe78fc1/operations/5a3044f608fa5e06b890f164
+
+'''
+
+project_name_to_id = {'TNC': '11111111',
+                      'BU': '22222222',
+                      'YUNNAN': '33333333'}
+
+SERVICE_NAME = 'tncapi'
+API_VERSION = 'v1.0'
+END_POINT_NAME = 'Prediction'
+MODEL_NAME = '21CResNet18'
+
+# Clean up temp image folder
+temp_folder = os.path.join(os.getcwd(), 'temp')
+if os.path.exists(temp_folder):
+    shutil.rmtree(temp_folder)
+    os.makedirs(temp_folder)
+
+model = BU_ModelLoader.FRCNN_Model('14CFRCNNAlexNet')
+model.load()
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+# 1.	PredictImageUrl
+# https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/{projectId}/url[?iterationId][&application]
+predict_image_url_endpoint = "/" + SERVICE_NAME + \
+                             "/" + API_VERSION + \
+                             "/" + END_POINT_NAME + \
+                             "/" + project_name_to_id['BU'] + \
+                             "/url"
+print("PredictImageUrl API = ", predict_image_url_endpoint)
+
+
+@app.route(predict_image_url_endpoint, methods=['POST'])
+def post_prediction_img_url():
+    # Print received request for debugging purpose
+    # TODO: create a logger for basic information
+
+    print("=== Arguments ===")
+    iteration_id = request.args.get('iterationId')
+    print("iteration_id = {!s}".format(iteration_id))
+    application = request.args.get('application')
+    print("application = {!s}".format(application))
+    print("=== Headers ===")
+    content_type = request.headers.get('Content-Type')
+    print("Content-Type = {!s}".format(content_type))
+
+    # TODO: we can use Prediction-Key as model chooser
+    prediction_key = request.headers.get('Prediction-Key')
+    print("Prediction-Key = {!s}".format(prediction_key))
+    print("=== Body ===")
+    img_url = request.json
+    print("Url = {!s}".format(img_url))
+
+    # Download image file from given Url, if failed to downloading the image simply return error code    # TODO: check if the file is really an image file before downloading
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+    filename = img_url[img_url.rfind('/')+1:]
+    img_path_file = os.path.join(temp_folder, filename)
+
+    r = requests.get(img_url, allow_redirects=True, verify=False, auth=('user', 'pass'))
+    if r.status_code == requests.codes.ok:
+        with open(img_path_file, 'wb') as f:
+            f.write(r.content)
+    else:
+        return jsonify({"Url": img_url, "Code": r.status_code, "Error": r.reason})
+
+    if not os.path.exists(img_path_file):
+        res_error = {"Url": img_url, "Error": "Can not open image file."}
+        return jsonify(res_error)
+
+    predictions = model.predict(img_path_file)
+    os.remove(img_path_file)
+
+    res_prediction_img_url = {
+        "Id": "string",
+        "Project": project_name_to_id['BU'],
+        "Iteration": iteration_id,
+        "Created": datetime.datetime.now().isoformat(),
+        "Predictions": predictions}
+    return jsonify(res_prediction_img_url)
+
+
+# 2.	PredictImage
+# https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/{projectId}/image[?iterationId][&application]
+predict_image_endpoint = "/" + SERVICE_NAME + \
+                             "/" + API_VERSION + \
+                             "/" + END_POINT_NAME + \
+                             "/" + project_name_to_id['BU'] + \
+                             "/image"
+print("PredictImage API = ", predict_image_endpoint)
+
+
+@app.route(predict_image_endpoint, methods=['POST'])
+def post_prediction_image():
+    # Print received request for debugging purpose
+    # TODO: create a logger for basic information
+
+    print("=== Arguments ===")
+    iteration_id = request.args.get('iterationId')
+    print("iteration_id = {!s}".format(iteration_id))
+    application = request.args.get('application')
+    print("application = {!s}".format(application))
+    print("=== Headers ===")
+    content_type = request.headers.get('Content-Type')
+    print("Content-Type = {!s}".format(content_type))
+    content_length = request.headers.get('Content-Length')
+    print("Content-Length = {!s}".format(content_length))
+
+    # TODO: we can use Prediction-Key as model chooser
+    prediction_key = request.headers.get('Prediction-Key')
+    print("Prediction-Key = {!s}".format(prediction_key))
+    print("=== Saving content to temp file ===")
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+    temp_file = tempfile.NamedTemporaryFile(suffix='.JPG', dir=temp_folder, delete=False)
+    img_path_file = temp_file.name
+    temp_file.write(request.get_data())
+    temp_file.close()
+
+    print("Image is saved as {!s}".format(img_path_file))
+    predictions = model.predict(img_path_file)
+    os.remove(img_path_file)
+    print("Image file {!s} is removed.".format(img_path_file))
+
+    res_prediction_img_url = {
+        "Id": "string",
+        "Project": project_name_to_id['TNC'],
+        "Iteration": iteration_id,
+        "Created": datetime.datetime.now().isoformat(),
+        "Predictions": predictions}
+
+    return jsonify(res_prediction_img_url)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+app.run(ssl_context='adhoc')
